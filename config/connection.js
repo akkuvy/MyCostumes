68fb0263be42b54478cd82a6a49966c18e2af875
akkuvy <vyshnuakku@gmail.com>
2021-01-14 22:31:09 +0530

    Nav bar errors fixed

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state = {
   db: null,
 };
 module.exports.connect = function (done) {
-  const url = "mongodb://localhost:27017";
+  const url ='mongodb://localhost:27017' 
   const dbname = "MyCostumes";
 
   mongoClient.connect(url, { useUnifiedTopology: true }, (err, data) => {
